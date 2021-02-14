f220ef3ac0653fdf4d994c9e3a78005d1b06bfab
EC2 Default User <aka435304@gmail.com>
2021-02-14 19:08:38 +0000

    [finish]解答すり合わせ

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -16,70 +16,64 @@
           end
           context '表示の確認' do
             it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
-              expect(page).to have_link "", href: lists_path
+              expect(page).to have_content 'ここはTopページです'
             end
             it 'top_pathが"/top"であるか' do
-              expect(current_path).to eq('/')
+              expect(current_path).to eq('/top')
             end
           end
         end
+
         describe '投稿画面のテスト' do
           before do #投稿画面への遷移
             visit todolists_new_path
           end
           context '表示の確認' do
             it 'todolists_new_pathが"/todolists/new"であるか' do
-              expect(todolists_new_path).to eq('/todolists/new')
+              expect(current_path).to eq('/todolists/new')
             end
             it '投稿ボタンが表示されているか' do
-              expect(page).to have_botton 'Create Book'
+              expect(page).to have_botton '投稿'
             end
           end
           context '投稿処理のテスト' do
             it '投稿後のリダイレクト先は正しいか' do
-              fill_in 'lists[title]', with: Faker::Lorem.characters(number:1)
-              fill_in 'lists[body]', with: Faker::Lorem.characters(number:1)
+              fill_in 'lists[title]', with: Faker::Lorem.characters(number:5)
+              fill_in 'lists[body]', with: Faker::Lorem.characters(number:20)
               click_button '投稿'
-              expect(page).to have_current_path todolist_path(list.id)
+              expect(page).to have_current_path todolist_path(List.last)
               # list.idもlistも同じ意味
             end
           end
         end
-        describe '一覧画面のテスト' do
+        describe '投稿一覧のテスト' do
           before do  #一覧画面への遷移
             visit todolists_path
           end
-          context '一覧の表示とリンクの確認'
-          it "一覧表示画面に投稿されたものが表示されているか"
-            (1..5).each do |i|
-              List.create(title:'hoge'+i.to_s,body:'body'+1.to_s)
-            end
-            visit todolists_path
-            List.all.each_with_index do |list,i|
-              j = i * 3
+          context '表示の確認' do
+           it "投稿されたものが表示されているか" do
               expect(page).to have_content list.title
-              expect(page).to have_content book.body
-            end
+              expect(page).to have_link list.title
+           end
+          end
         end
         describe '詳細画面のテスト' do
-          before #詳細画面への遷移
-            visit list_path(list)
+          before do #詳細画面への遷移
+            visit todolist_path(list)
           end
-          context '表示のテスト' do
+          context '表示の確認' do
             it '削除リンクが存在しているか' do
-              destroy_link = find_all('a')[0]
-              expect(destroy_link.native.inner_text).to match(/destroy/i)
+              expect(page).to have_link '削除'
             end
             it '編集リンクが存在しているか' do
-              show_link = find_all('a')[0]
-              expect(edit_link.native.inner_text).to match(/edit/i)
+              expect(page).to have_link '編集'
             end
           end
           context 'リンクの遷移先の確認' do
-            it '編集の遷移先は編集画面か'
+            it '編集の遷移先は編集画面か' do
               edit_link = find_all('a')[0]
               edit_link.click
-              expect(current_path).to eq('/books') + book.id.to_s + ('/edit')
+              expect(current_path).to eq('/todolists/' + list.id.to_s + '/edit')
             end
           end
           context 'list削除のテスト' do
@@ -87,14 +81,16 @@
               expect{ list.destroy }.to change{ List.count }.by(-1)
             end
           end
+        end
+
           describe '編集画面のテスト' do
             before do #編集画面への遷移
               visit edit_todolist_path(list)
             end
             context '表示の確認' do
               it '編集前のタイトルと本文がフォームに表示(セット)されている' do
-                expect(page).to have_field 'todolist[:title]', with: todolist.title
-                expect(page).to have_field 'todolist[:body]', with: todolist.body
+                expect(page).to have_field 'list[:title]', with: list.title
+                expect(page).to have_field 'list[:body]', with: list.body
               end
               it '保存ボタンが表示される' do
                 expect(page).to have_botton '保存'
@@ -106,6 +102,9 @@
                 fill_in 'todolist[body]', with: Faker::Lorem.characters(number:20)
                 click_button '保存'
                 expect(page).to have_current_path todolist_path(list)
-          
-                
-      
+              end
+            end
+          end
+        end
+
+
